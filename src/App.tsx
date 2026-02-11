07df3e1f456f78de64ce672334a08d0c7527d3e2
Edward Montero <leobastian562@gmail.com>
2026-02-11 11:10:38 -0500

    chore: Calendar and history integrations

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import Sessions from './pages/Sessions';
 import { useAuth } from './hooks/useAuth';
 
 function AppRoutes() {
@@ -20,6 +21,11 @@ function AppRoutes() {
         <Route path="/" element={
           <Layout>
             <Dashboard />
+          </Layout>
+        } />
+        <Route path="/sessions" element={
+          <Layout>
+            <Sessions />
           </Layout>
         } />
         <Route path="/dashboard" element={<Navigate to="/" replace />} />
