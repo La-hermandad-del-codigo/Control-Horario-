a457a527280fb595603c5cb70d7ee9e2744cde49
Gustavo Tuesta <gustavo.tuesta.puse@gmail.com>
2026-02-20 10:27:08 -0500

    Verificion de error

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -82,7 +82,7 @@ export function useAuth() {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin,
+                redirectTo: `${window.location.origin}/`,
             },
         });
         if (error) throw error;
